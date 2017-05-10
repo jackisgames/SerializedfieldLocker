bc161a48d3959fab406e0ba282f5cc6a545b4990
Jacky Riawan <jackisgames@gmail.com>
2017-05-10 23:47:08 +0100

    Initial commit

---
+++ LockAttribute.cs
@@ -0,0 +1,105 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+
+internal class LockAttribute : PropertyAttribute
+{
+    internal string Pin;
+
+    internal LockAttribute()
+    {
+        Pin = string.Empty;
+    }
+
+    internal LockAttribute(string pin = "")
+    {
+        Pin = pin;
+    }
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(LockAttribute))]
+class LockAttributeDrawer : PropertyDrawer
+{
+    private bool isUnlocked = false;
+    private bool enterPin = false;
+    private string passErrorMessage = string.Empty;
+    private string passcode;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if (enterPin)
+            EnterPinGUI(position, property, label);
+        else
+            LockGUI(position, property, label);
+        //GUI.Box(position, "Locked");
+        //base.OnGUI(position, property, label);
+    }
+
+    private void EnterPinGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        Rect buttonSubmitRect = new Rect(position);
+        Rect buttonCancelRect = new Rect(position);
+
+        position.width *= .7f;
+
+        buttonCancelRect.x += position.width;
+        buttonCancelRect.width = (buttonCancelRect.width - position.width)*.5f;
+
+        buttonSubmitRect.x = buttonCancelRect.x + buttonCancelRect.width;
+        buttonSubmitRect.width = buttonCancelRect.width;
+
+        passcode = EditorGUI.PasswordField(position, string.Format("Passcode {0}", passErrorMessage), passcode);
+        if (GUI.Button(buttonCancelRect, "Cancel"))
+        {
+            enterPin = false;
+        }
+        if (GUI.Button(buttonSubmitRect, "Submit"))
+        {
+            if (passcode.Equals(PIN))
+            {
+                isUnlocked = true;
+                enterPin = false;
+            }
+            else
+            {
+                passErrorMessage = "Wrong password";
+            }
+        }
+    }
+
+    private void LockGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        Rect buttonRect = new Rect(position);
+
+        position.width *= .85f;
+
+        buttonRect.x += position.width;
+        buttonRect.width = buttonRect.width - position.width;
+
+        GUI.enabled = isUnlocked;
+
+        EditorGUI.PropertyField(position, property);
+
+        GUI.enabled = true;
+        if (GUI.Button(buttonRect, isUnlocked ? "Lock" : "Unlock"))
+        {
+            if (string.IsNullOrEmpty(PIN))
+                isUnlocked = !isUnlocked;
+            else
+                enterPin = true;
+            passErrorMessage = passcode = string.Empty;
+
+        }
+    }
+
+    private string PIN
+    {
+        get { return ((LockAttribute) attribute).Pin; }
+
+    }
+}
+#endif
+
---
+++ LockExample.cs
@@ -0,0 +1,13 @@
+﻿using Helpers;
+using UnityEngine;
+
+class LockExample:MonoBehaviour
+{
+    [Header("Magic Numbers Please do not touch!")]
+    [SerializeField]
+    private int noLockVariable;
+    [SerializeField,Lock]
+    private int noPinLockedVariable;
+    [SerializeField,Lock("111")]
+    private int withPinLockedVariable;
+}
